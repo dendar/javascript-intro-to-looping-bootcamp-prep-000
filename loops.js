f935ae12c0931bba014935a48fdc2be96edc5984
Nadir Sarigul <nadir.nyc62@gmail.com>
2019-01-31 02:07:21 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,5 +1,20 @@
-let  countdown = 50
-while (countdown>0){
-  console.log(--countdown)
-  
+
+function forLoop(array){
+  for(let i =0; i<25; i++){
+    if (i===1) {
+      array.push("I am ${i} dtrange loops.")
+      
+    }
+  }
+  return array
 }
+
+
+
+
+
+// let  countdown = 50
+// while (countdown>0){
+//   console.log(--countdown)
+//   
+//}
