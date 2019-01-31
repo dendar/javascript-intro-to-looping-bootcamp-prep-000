9fb889e077644a6dd8e58bd87e5756ed7ef69a80
Nadir Sarigul <nadir.nyc62@gmail.com>
2019-01-31 02:13:59 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,27 +1,17 @@
-//
-// function forLoop(array){
-//   for(let i =0; i < 25; i++){
-//     if (i === 1) {
-//       array.push("I am 1 dtrange loop.")
-//
-//     }
-//     else{
-//       array.push("I am ${i}) strange loops.")
-//     }
-//   }
-//   return array
-// }
-function forLoop(array) {
-  for (let i = 0; i < 25; i++) {
+
+function forLoop(array){
+  for(let i =0; i < 25; i++){
     if (i === 1) {
-      array.push("I am 1 strange loop.");
+      array.push("I am 1 dtrange loop.")
+
     }
-    else {
-      array.push(`I am ${i} strange loops.`);
+    else{
+      array.push(`I am ${i}) strange loops.`)
     }
   }
-  return array;
+  return array
 }
+
 
 
 
